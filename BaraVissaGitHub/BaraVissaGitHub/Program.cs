a5a5d59aacf67c7dfdf85bb154ced5ae5a89ab5c
Eric <nilssonericnilsson@gmail.com>
2019-09-16 10:53:16 +0200

    Heeyo

---
+++ BaraVissaGitHub/BaraVissaGitHub/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaraVissaGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HEJ!");
+        }
+    }
+}
