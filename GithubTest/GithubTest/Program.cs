60afa50fd65ef1ea05f012527a58a560edc36413
Eric <nilssonericnilsson@gmail.com>
2019-09-16 11:03:28 +0200

    Cha

--- GithubTest/GithubTest/Program.cs
+++ GithubTest/GithubTest/Program.cs
@@ -12,6 +12,7 @@ namespace GithubTest
         {
             Console.WriteLine("Helloworld");
             Console.WriteLine("Hello2");
+            Console.WriteLine("Test3");
         }
     }
 }
